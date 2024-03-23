5122ad111ac7f7ba346b3fd82646bd04e28d1115
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2024-03-22 22:16:17 -0400

    Add capacitor boilerplate.

---
+++ src/components/FileInput.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const currentFolder = ref('')
+</script>
+
+<template>
+    <select name="folder" id="folder-select">
+        <option value="">{{ currentFolder != '' ? currentFolder : 'Choose folder' }}</option>
+        <option value="new">New folder</option>
+    </select>
+    <div>
+        <input
+        type="file"
+        @change="onFileChanged($event)"
+        accept="image/*"
+        capture
+        />
+    </div>
+</template>
